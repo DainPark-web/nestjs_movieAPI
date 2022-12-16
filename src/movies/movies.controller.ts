f8338eeefdca18279e5bdfcb2387697ee95a5242
dain <57316771+DainPark-web@users.noreply.github.com>
2022-12-16 19:21:55 +0100

    update

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
-@Controller()
+@Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
@@ -8,7 +8,22 @@ export class MoviesController {
   }
 
   @Get('/:id')
-  getOne() {
-    return 'This will return One Movie';
+  getOne(@Param('id') id: string) {
+    return `get movies param: ${id}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `Delete Movie ${movieId}`;
+  }
+
+  @Patch('/:id')
+  update(@Param('id') movieId: string) {
+    return `Update Movie ${movieId}`;
   }
 }
