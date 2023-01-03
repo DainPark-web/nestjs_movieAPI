18f567e2763aa82020c3e174a52dc337f6d5f345
dain <57316771+DainPark-web@users.noreply.github.com>
2023-01-03 12:04:29 +0100

    update

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,7 +16,33 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it('should be 4', () => {
-    expect(2 + 2).toEqual(4);
+
+  describe('getAll', () => {
+    it('should return a array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+  describe('getOne', () => {
+    it('It should return a Movie', () => {
+      service.create({
+        title: 'test',
+        genres: ['test'],
+        year: 1222,
+      });
+      const result = service.getOne(1);
+      expect(result).toBeDefined();
+      expect(result.id).toEqual(1);
+      expect(result.title).toEqual('test');
+    });
+    // 에러 발견시
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Movie with ID: ${999} not Found`);
+      }
+    });
   });
 });
