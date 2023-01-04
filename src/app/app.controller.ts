db6b2443948a8b04173b3f4b3bfdf74fa8b3c159
dain <57316771+DainPark-web@users.noreply.github.com>
2023-01-04 22:03:41 +0100

    update

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Req, Res } from '@nestjs/common';
 @Controller('')
 export class AppController {
   @Get()
-  home(@Req() req, @Res() res) {
+  home() {
     return 'Welcome to my Movie API';
   }
 }
